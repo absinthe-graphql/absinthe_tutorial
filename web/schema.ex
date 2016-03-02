6a7dd544f08dc83c270fd8482b91d861c70ea6a5
Bruce Williams <brwcodes@gmail.com>
2016-03-01 22:53:57 -0800

    Fix typo in schema causing compilation error
    When we switched to the new notation two arguments were
    errantly entered as fields and caught by schema compilation
    checks. This fixes this issue.
    
    Closes #1, #3.

--- web/schema.ex
+++ web/schema.ex
@@ -24,8 +24,8 @@ defmodule Blog.Schema do
     end
 
     field :user, :user do
-      field :contact, non_null(:contact_input)
-      field :password, :string
+      arg :contact, non_null(:contact_input)
+      arg :password, :string
 
       resolve &Resolver.User.create/2
     end
