695ff5eae896ff4b8ed570534d0d91f58d6726c9
Chris Nelson <chris.nelson@kroger.com>
2016-07-02 13:26:04 -0400

    Put in graphiql so I can figure out what's going on

--- web/router.ex
+++ web/router.ex
@@ -5,9 +5,11 @@ defmodule Blog.Router do
     plug Blog.Context
   end
 
-  scope "/api" do
+  scope "/" do
     pipe_through :graphql
 
-    forward "/", Absinthe.Plug, schema: Blog.Schema
+    get "/graphiql", Absinthe.Plug.GraphiQL, schema: Blog.Schema
+    post "/graphiql", Absinthe.Plug.GraphiQL, schema: Blog.Schema
+    forward "/graphql", Absinthe.Plug, schema: Blog.Schema
   end
 end
