fafe96abebcdca2e0beaea7da21feac908cfd70b
xieyiming <mydearxym@gmail.com>
2017-12-05 23:24:15 +0800

    try to fix #4

--- lib/blog_web/schema/content_types.ex
+++ lib/blog_web/schema/content_types.ex
@@ -1,12 +1,15 @@
 defmodule BlogWeb.Schema.ContentTypes do
   use Absinthe.Schema.Notation
+  alias BlogWeb.Resolvers
 
   @desc "A blog post"
   object :post do
     field :id, :id
     field :title, :string
     field :body, :string
-    field :author, :user
+    field :author, :user do
+      resolve &Resolvers.Accounts.find_user/3
+    end
     field :published_at, :naive_datetime
   end
 
