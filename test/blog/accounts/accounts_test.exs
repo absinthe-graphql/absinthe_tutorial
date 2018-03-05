ec357ca061e1a38eac6eab50a122780ebfbd2e33
smaximov <s.maximov@teachmeplease.com>
2018-03-05 17:24:49 +0300

    Validate uniqueness on (type, value) columns of the `contacts` table

--- test/blog/accounts/accounts_test.exs
+++ test/blog/accounts/accounts_test.exs
@@ -15,4 +15,13 @@ defmodule Blog.AccountsTest do
     assert {:error, %Ecto.Changeset{}} = Accounts.create_user(attrs)
     assert ^contact_count = Repo.aggregate(Accounts.Contact, :count, :id)
   end
+
+  test "create_user/1 discards duplicate contacts" do
+    contact = %{type: "phone", value: "+1 000 000 0000"}
+
+    assert {:ok, _} = Accounts.create_user(%{contact: contact, name: "name", password: "password"})
+
+    assert {:error, changeset} = Accounts.create_user(%{contact: contact, name: "other", password: "password"})
+    assert "has already been taken" in errors_on(changeset).value
+  end
 end
